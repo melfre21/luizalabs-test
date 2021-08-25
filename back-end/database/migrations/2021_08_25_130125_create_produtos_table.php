56472ec511eaece560464f4899e8c4bd27ca3d11
Melfre Diego R S <melfre21@hotmail.com>
2021-08-25 16:26:33 -0300

    Enviando model e migrations iniciais

---
+++ back-end/database/migrations/2021_08_25_130125_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo', 20);
+            $table->string('nome', 20);
+            $table->string('fabricacao', 20);
+            $table->string('tamanho', 20);
+            $table->decimal('valor', 10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
